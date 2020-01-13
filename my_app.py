18056880279098d1d75f3d2a43ef9f5772750440
sudarshan037 <sudarshan037@gmail.com>
2020-01-13 07:17:10 +0000

    all done

--- my_app.py
+++ my_app.py
@@ -1,13 +1,21 @@
 from flask import Flask
+#from flask.ext.sqlalchemy import SQLAlchemy
+import mysql.connector
+
+mydb = mysql.connector.connect(host='localhost', user='uname', passwd='password', database='assignment1')
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' %name
+#for SQLAlchemy mysql://username:password@hostname/database
+
+@app.route('/')
+def main():
+    mycursor = mydb.cursor()
+    query = "SELECT table1.Pincode, SUM(table2.Population) FROM table1 INNER JOIN table2 ON table1.Office_ID=table2.Office_ID WHERE table1.Cities='Jaipur' GROUP BY table1.Pincode;"
+    mycursor.execute(query)
+    result = mycursor.fetchall()
+    return str(result)
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='0.0.0.0',port=8000)
